ac005d4ad721cd264c58ebbdf37f370bb61d3b97
Dogkaebi <st9dog@gmail.com>
2022-07-27 16:22:55 +0900

    backend 로그인 기능

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require("express");
+const bodyPaser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -11,6 +12,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyPaser.json());
+app.use(bodyPaser.urlencoded({ extended: true}));
 
 app.use("/", home); // 미들웨어 등록 메서드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,39 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/login");
+const output = {
+    home: (req, res) => {
+        res.render("home/login");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
+
+const users = {
+    id: ["ironman", "captain", "thor"],
+    pw: ["123", "234", "345"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+              pw = req.body.pw;
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx] === pw) {
+                return res.json({
+                    success: true,
+                })
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인 실패",
+        })
+    },
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
